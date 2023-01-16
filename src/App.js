038aae2505c66eef46c3ca5c563d58ce28cecc74
Gaurav kumar <ergauravkumar02071998@gmail.com>
2023-01-16 20:52:58 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
  
 import './App.css';
-
+import { AllRoutes } from './AllRoutes/AllRoutes';
 function App() {
   return (
-    <div className="App">
+    < >
+      <AllRoutes/>
       
-    </div>
+    </>
   );
 }
 
